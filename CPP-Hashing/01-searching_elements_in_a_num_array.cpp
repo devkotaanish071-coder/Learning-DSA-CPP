9f3bedca98fce2f7356527fbf35cd9f3c892487f
Anish Devkota <devkotaanish071@gmail.com>
2026-06-20 10:11:42 +0530

    Learning Hashing

---
+++ CPP-Hashing/01-searching_elements_in_a_num_array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //Taking array size from user
+    int number;
+    cout << "Enter the size of array:";
+    cin >> number;
+
+    //Taking array elements
+    int array[number];
+    cout << "Enter array elements:";
+    for(int i = 0; i < number; i++){
+        cin >> array[i];
+    }
+
+    //Declaring hash with 0
+    int hash[10] = {0};
+    for(int i = 0; i < number; i++){
+        hash[array[i]] += 1;
+    }
+
+    int search;
+    cout << "Enter the number of searches:";
+    cin >> search;
+    while(search--){
+        int num;
+        cout << "Enter a number to search:";
+        cin >> num;
+        cout << hash[num] << endl;
+    }
+
+    return 0;
+}
