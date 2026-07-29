0f4e459685aed5064e6fe9b020b26bfe6f556704
Anish Devkota <devkotaanish071@gmail.com>
2026-07-29 23:01:27 +0530

    Added Intersection of 2 Sorted Array

--- 06-CPP-Arrays/01-Basic_Array/15-intersection_of_2_sorted_array_optimized.cpp
+++ 06-CPP-Arrays/01-Basic_Array/15-intersection_of_2_sorted_array_optimized.cpp
@@ -1,3 +1,30 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+vector<int> intersection_of_2_missing_number(){
+
+}
+
+int main(){
+
+    int size1;
+    cout << "Enter the size of first Array:";
+    cin >> size1;
+
+    vector<int> array1(size1);
+    cout << "Enter First array elements:";
+    for(int i = 0; i < size1; i++){
+        cin >> array1[i];
+    }
+
+    int size2;
+    cout << "Enter second array size:";
+    cin >> size2;
+
+    vector<int> array2(size2);
+    cout << "Enter second array elements:";
+    for(int i = 0; i < size2; i++){
+        cin >> array2[i];
+    }
+}
